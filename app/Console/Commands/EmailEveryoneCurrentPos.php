baf3f6e7652e311fc10c734dc73969efef56e47c
Harry Messenger <hsmess@Harrys-MacBook-Pro-3.local>
2021-04-01 09:53:15 +0100

    test tags

---
+++ app/Console/Commands/EmailEveryoneCurrentPos.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Notifications\EmailTagPos;
+use Illuminate\Console\Command;
+
+class EmailEveryoneCurrentPos extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'everyone:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $players = User::where('paid_2021',true)->get()->each(function ($user){
+            $user->notify(new EmailTagPos($user->current_tag_position));
+        });
+    }
+}
---
+++ app/Notifications/EmailTagPos.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EmailTagPos extends Notification
+{
+    use Queueable;
+    protected $ctp = null;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($ctp)
+    {
+       $this->ctp = $ctp;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Welcome to the Manchester BagTag 2021 Season')
+                    ->line('Tags have now been assigned, and your starting tag is: ' . $this->ctp)
+                    ->line('If you selected shipping, this will be mailed tomorrow, and if not, you can collect your tag from the container by meeting anyone who has keys')
+                    ->line('Also, the new dashboard where the current tags are listed, and where you can update your tag when you trade is now available.')
+                    ->action('Check it out!', url('/'))
+                    ->line('Thanks for signing up, and making this our biggest "tags" year ever, already!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
